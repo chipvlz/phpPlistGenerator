bced0811cd3558bf187d060b3e945cccfa8880ad
Alex Fang <frjalex@gmail.com>
2015-07-11 22:55:28 +0800

    default MIME type change

--- plistGenerator/_header.php
+++ plistGenerator/_header.php
@@ -1,6 +1,6 @@
 <?php
 
 //custom php header for MIME type. Required for serving Plist files.
-header('Content-type: application/x-apple-aspen-config; chatset=utf-8');
+//header('Content-type: application/x-apple-aspen-config; chatset=utf-8');
 
 ?>
