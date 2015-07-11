0abec22b7e7f723b378cc5a9b95bf8148b39658f
Alex Fang <frjalex@gmail.com>
2015-07-11 19:33:54 +0800

    convert style 'get' to 'post'

--- plistGenerator/_getVars.php
+++ plistGenerator/_getVars.php
@@ -6,24 +6,24 @@ require_once '_header.php';
 //get the imported variables using $_GET from html input
 
 //global vars
-$mode = $_GET['mode'];
-$declaration = $_GET['declaration'];
+$mode = $_POST['mode'];
+$declaration = $_POST['declaration'];
 $currentString = generateRandomString();//random string
 $iosResponseContent = iPhoneResponseFetcher(); //retrieves the iOS response in DER encoding
 
 //vars for vpnPlistGenerator
-$server =  $_GET['server'];     //$node->Server();
-$pass = $_GET['pass'];          //$oo->get_pass();
-$username = $_GET['username'];  //$oo->get_port();
+$server =  $_POST['server'];     //$node->Server();
+$pass = $_POST['pass'];          //$oo->get_pass();
+$username = $_POST['username'];  //$oo->get_port();
 
 //vars for ldapPlistGenerator
-$ldapserverprefix = $_GET['ldapserverprefix'];
-$ldapserversuffix = $_GET['ldapserversuffix'];//apple has merely strange requirements on these, seperating root domain and 2nd level domains. I do not have very much documentations on this, but if anyone had a more artistic 
+$ldapserverprefix = $_POST['ldapserverprefix'];
+$ldapserversuffix = $_POST['ldapserversuffix'];//apple has merely strange requirements on these, seperating root domain and 2nd level domains. I do not have very much documentations on this, but if anyone had a more artistic 
 //solution, I would be very happy to fix.
 $ldapserver = $ldapserverprefix.'.'.$ldapserversuffix; //use this merely strange way to get fqdn of ldap server. Documentation on this strongly needed.
-$smtpserver = $_GET['mailserver']; //assuming SMTP IMAP and POP3 servers are on the same address 
-$mailusername = $_GET['mailusername'];
-$mailpassword = $_GET['mailpassword'];
+$smtpserver = $_POST['mailserver']; //assuming SMTP IMAP and POP3 servers are on the same address 
+$mailusername = $_POST['mailusername'];
+$mailpassword = $_POST['mailpassword'];
 $mailuseraddress = $mailusername.'@'.$mailserver;
 
 ?>
