d2dc343e6143a8faedfa3b1baf17db5e03ccaf79
Alex Fang <frjalex@gmail.com>
2015-07-15 17:57:05 +0800

    Update _header.php

--- plistGenerator/_header.php
+++ plistGenerator/_header.php
@@ -1,6 +1,7 @@
 <?php
 
 //custom php header for MIME type. Required for serving Plist files.
-//header('Content-type: application/x-apple-aspen-config; chatset=utf-8');
-
+header('Content-type: application/x-apple-aspen-config; chatset=utf-8');
+header('Content-Disposition: attachment; filename="generated.mobileconfig"');
+echo $mobileconfig;
 ?>
